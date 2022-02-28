b76a4b5887743ce24fdf532c2c63a97d455aac71
Eric Hu <huericnan@gmail.com>
2022-02-28 13:52:11 -0500

    initial commit

--- backend/database.ts
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/ecommerce";
-
-MongoClient.connect(url, function(err:any, db:any) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
