02aa90bda6c5119eb0abe1afc45da6822372576b
Eric Hu <huericnan@gmail.com>
2022-03-22 16:07:41 -0400

    frontend product page

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -1,70 +1,123 @@
 <script></script>
 
 <template>
-  <main class="flex flex-col">
-    <div class="flex flex-col md:flex-row">
-      <div class="basis-3/4 md:mr-2">
+  <main class="flex flex-col space-y-4 mx-2 md:mx-0">
+    <div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+      <div class="basis-3/4">
         <div
-          class="carousel carousel-center w-full p-4 space-x-2 bg-neutral rounded-md"
+          class="carousel w-full h-full p-4 space-x-4 bg-neutral rounded-md md:mr-2"
         >
-          <div id="slide1" class="carousel-item relative w-full">
+          <div class="carousel-item">
             <img
-              src="https://api.lorem.space/image/car?w=800&h=400&hash=8B7BCDC2"
-              class="w-full rounded-md"
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=8B7BCDC2"
+              class="rounded-md h-full"
             />
-            /
-            <div
-              class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-            >
-              <a href="#slide4" class="btn btn-circle">❮</a>
-              <a href="#slide2" class="btn btn-circle">❯</a>
-            </div>
           </div>
-          <div id="slide2" class="carousel-item relative w-full">
+          <div class="carousel-item">
             <img
-              src="https://api.lorem.space/image/car?w=800&h=400&hash=500B67FB"
-              class="w-full rounded-md"
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=500B67FB"
+              class="rounded-md h-full"
             />
-            /
-            <div
-              class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-            >
-              <a href="#slide1" class="btn btn-circle">❮</a>
-              <a href="#slide3" class="btn btn-circle">❯</a>
-            </div>
           </div>
-          <div id="slide3" class="carousel-item relative w-full">
+          <div class="carousel-item">
             <img
-              src="https://api.lorem.space/image/car?w=800&h=400&hash=A89D0DE6"
-              class="w-full rounded-md"
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=A89D0DE6"
+              class="rounded-md h-full"
             />
-            /
-            <div
-              class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-            >
-              <a href="#slide2" class="btn btn-circle">❮</a>
-              <a href="#slide4" class="btn btn-circle">❯</a>
-            </div>
           </div>
-          <div id="slide4" class="carousel-item relative w-full">
+          <div class="carousel-item">
             <img
-              src="https://api.lorem.space/image/car?w=800&h=400&hash=225E6693"
-              class="w-full rounded-md"
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=225E6693"
+              class="rounded-md h-full"
             />
-            /
-            <div
-              class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-            >
-              <a href="#slide3" class="btn btn-circle">❮</a>
-              <a href="#slide1" class="btn btn-circle">❯</a>
-            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=9D9539E7"
+              class="rounded-md h-full"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=BDC01094"
+              class="rounded-md h-full"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://api.lorem.space/image/furniture?w=250&h=180&hash=7F5AE56A"
+              class="rounded-md h-full"
+            />
           </div>
         </div>
       </div>
-      <div class="basis-1/4 md:ml-2 flex flex-col">
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
-        <div>
-          <div class="rating">
+      <div class="basis-1/4 flex flex-col space-y-8">
+        <h2 class="font-bold uppercase text-2xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit
+        </h2>
+        <div class="flex">
+          <div class="rating mr-2">
+            <input
+              type="radio"
+              name="rating-1"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              class="mask mask-star-2 bg-orange-400"
+              checked
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              class="mask mask-star-2 bg-orange-400"
+            />
+          </div>
+          <a class="hover:underline hover:text-primary">7 Reviews</a>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+          accusantium a ipsam, cupiditate perferendis earum ipsum at. Ducimus
+          cum facere doloribus tempora explicabo ex aliquid ratione consequatur.
+          Ipsa, corrupti modi.
+        </p>
+        <div class="form-control">
+          <div class="input-group">
+            <button class="btn btn-square btn-outline btn-primary">-</button>
+            <input
+              type="text"
+              placeholder="Choose the amount..."
+              class="input input-bordered w-full"
+            />
+            <button class="btn btn-square btn-outline btn-primary">+</button>
+          </div>
+        </div>
+        <button class="btn btn-primary">Add to Cart</button>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="flex flex-col space-y-8">
+      <h2 class="font-bold text-2xl">Reviews</h2>
+      <div class="flex flex-col space-y-2">
+        <div class="flex space-x-4 items-center">
+          <div class="avatar">
+            <div class="w-12 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=92310" />
+            </div>
+          </div>
+          <p class="font-semibold text-lg">username</p>
+          <div class="rating rating-sm">
             <input
               type="radio"
               name="rating-2"
@@ -93,9 +146,107 @@
             />
           </div>
         </div>
-        <p>7 Reviews</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, quo
+          nulla. Reprehenderit velit molestiae adipisci, impedit quae soluta
+          commodi dicta nulla accusamus suscipit eligendi a repellat non
+          quisquam culpa quidem. Veniam, doloremque obcaecati repellendus
+          corporis eos odio nisi. Perferendis quasi cupiditate nulla modi quas
+          alias nostrum rem labore quaerat quisquam.
+        </p>
+      </div>
+      <div class="flex flex-col space-y-2">
+        <div class="flex space-x-4 items-center">
+          <div class="avatar">
+            <div class="w-12 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=92310" />
+            </div>
+          </div>
+          <p class="font-semibold text-lg">username</p>
+          <div class="rating rating-sm">
+            <input
+              type="radio"
+              name="rating-3"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-3"
+              class="mask mask-star-2 bg-orange-400"
+              checked
+            />
+            <input
+              type="radio"
+              name="rating-3"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-3"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-3"
+              class="mask mask-star-2 bg-orange-400"
+            />
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, quo
+          nulla. Reprehenderit velit molestiae adipisci, impedit quae soluta
+          commodi dicta nulla accusamus suscipit eligendi a repellat non
+          quisquam culpa quidem. Veniam, doloremque obcaecati repellendus
+          corporis eos odio nisi. Perferendis quasi cupiditate nulla modi quas
+          alias nostrum rem labore quaerat quisquam.
+        </p>
+      </div>
+      <div class="flex flex-col space-y-2">
+        <div class="flex space-x-4 items-center">
+          <div class="avatar">
+            <div class="w-12 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=92310" />
+            </div>
+          </div>
+          <p class="font-semibold text-lg">username</p>
+          <div class="rating rating-sm">
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-orange-400"
+              checked
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-orange-400"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-orange-400"
+            />
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, quo
+          nulla. Reprehenderit velit molestiae adipisci, impedit quae soluta
+          commodi dicta nulla accusamus suscipit eligendi a repellat non
+          quisquam culpa quidem. Veniam, doloremque obcaecati repellendus
+          corporis eos odio nisi. Perferendis quasi cupiditate nulla modi quas
+          alias nostrum rem labore quaerat quisquam.
+        </p>
       </div>
     </div>
-    <div></div>
   </main>
 </template>
